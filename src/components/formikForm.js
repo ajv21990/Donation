dffddbef1eda09848229caf2abf64d0718754d8d
AJ Villanueva <AJV21990@gmail.com>
2019-08-01 12:40:29 -0700

    Refactored donateForm

--- src/components/formikForm.js
+++ src/components/formikForm.js
@@ -1,6 +1,10 @@
 import React from "react";
 import { Formik } from "formik";
 import { Form } from '../components/formDonate'
+import CheckoutForm from '../components/CheckoutForm'
+import { Elements, StripeProvider } from 'react-stripe-elements'
+import Modal from '@material-ui/core/Modal'
+import axios from 'axios'
 import * as Yup from "yup";
 
 const validationSchema = Yup.object({
@@ -61,11 +65,57 @@ class formikForm extends React.Component {
             cvv: formInput.cvv,
             exp: formInput.exp,
         })
-        console.log(this.state)
-    };
+
+        const donor = {
+            firstName: this.state.fName,
+            lastName: this.state.lName,
+            email: this.state.email,
+            address: this.state.address,
+            apt: this.state.apt,
+            city: this.state.city,
+            state: this.state.state,
+            zip: this.state.zip,
+            amount: this.state.amount,
+            frequency: this.state.frequency,
+            cardNumber: this.state.cardNumber,
+            cvv: this.state.cvv,
+            exp: this.state.exp
+        }
+        axios.post(`http://localhost:8080/vapps/DonationsPage`, donor)
+            .then(res => {
+                console.log('Axios response', res)
+                if (res.status === 200) {
+                    //Do Stripe API Call
+                    this.chargeStripe(donor.amount)
+                }
+
+            })
+            .catch(error => {
+                console.log("error:", error)
+            })
+
+
+    }
+
+    chargeStripe = () => {
+        this.setState({
+            showModal: true
+        })
+
+    }
+
+    onClose = () => {
+        this.setState({
+            showModal: false
+        })
+        this.goToThank()
+    }
+
+    goToThank = () => {
+        this.props.history.push('/thank')
+    }
 
     render() {
-        // const classes = this.props;
         return (
             <React.Fragment>
                 <div>
@@ -75,6 +125,23 @@ class formikForm extends React.Component {
                         validationSchema={validationSchema}
                         onSubmit={this.submitValues}
                     />
+                </div>
+                <div>
+                    <Modal
+                        aria-labelledby="StripeModal"
+                        aria-describedby="Modal"
+                        open={this.state.showModal}
+                        onClose={this.onClose}
+                    >
+                        <StripeProvider apiKey=" pk_test_QKo9SBvX99oUe6wS8rYY4AKG00VXUbNidu">
+                            <div className="stripeModal">
+                                <h2>Your Donation Is Appreciated</h2>
+                                <Elements>
+                                    <CheckoutForm />
+                                </Elements>
+                            </div>
+                        </StripeProvider>
+                    </Modal>
                 </div>
             </React.Fragment>
         );
--- src/pages/donateForm.js
+++ src/pages/donateForm.js
@@ -1,10 +1,7 @@
 import React from 'react'
 import Header from '../components/header'
 import Footer from '../components/footer'
-import axios from 'axios'
-import CheckoutForm from '../components/CheckoutForm'
-import { Elements, StripeProvider } from 'react-stripe-elements'
-import Modal from '@material-ui/core/Modal'
+
 import FormikForm from '../components/formikForm'
 import '../stylesheets/formStyle.css'
 
@@ -12,77 +9,8 @@ export default class ContactForm extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            donationID: '',
-            fName: "",
-            lName: "",
-            email: "",
-            address: "",
-            apt: "",
-            city: "",
-            state: "",
-            zip: '',
-            amount: "",
-            frequency: "",
-            cardNumber: '',
-            cvv: '',
-            exp: "",
-            showModal: false
         }
     }
-
-    Submit = evt => {
-        evt.preventDefault()
-
-        const donor = {
-            firstName: this.state.fName,
-            lastName: this.state.lName,
-            email: this.state.email,
-            address: this.state.address,
-            apt: this.state.apt,
-            city: this.state.city,
-            state: this.state.state,
-            zip: this.state.zip,
-            amount: this.state.amount,
-            frequency: this.state.frequency,
-            cardNumber: this.state.cardNumber,
-            cvv: this.state.cvv,
-            exp: this.state.exp
-        }
-
-        axios.post(`http://localhost:8080/vapps/DonationsPage`, donor)
-            .then(res => {
-                console.log('Axios response', res)
-                if (res.status === 200) {
-                    //Do Stripe API Call
-                    this.chargeStripe(donor.amount)
-                }
-
-            })
-            .catch(error => {
-                console.log("error:", error)
-            })
-
-
-    }
-
-    chargeStripe = () => {
-        this.setState({
-            showModal: true
-        })
-
-    }
-
-    onClose = () => {
-        this.setState({
-            showModal: false
-        })
-        this.goToThank()
-    }
-    goToThank = () => {
-
-        this.props.history.push('/thank')
-    }
-
     render() {
 
         return (
@@ -97,23 +25,7 @@ export default class ContactForm extends React.Component {
                 </div>
                 <hr />
                 <Footer />
-                <Modal
-                    aria-labelledby="StripeModal"
-                    aria-describedby="Modal"
-                    open={this.state.showModal}
-                    onClose={this.onClose}
-                >
-                    <StripeProvider apiKey=" pk_test_QKo9SBvX99oUe6wS8rYY4AKG00VXUbNidu">
-                        <div className="stripeModal">
-                            <h2>Your Donation Is Appreciated</h2>
-                            <Elements>
-                                <CheckoutForm />
-                            </Elements>
-                        </div>
-                    </StripeProvider>
-                </Modal>
                 <br />
-
             </div>
 
         )
