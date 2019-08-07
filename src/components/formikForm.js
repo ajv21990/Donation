20f8de52780341341a87deaf757563dd88c61865
AJ Villanueva <AJV21990@gmail.com>
2019-08-07 15:03:39 -0700

    Removed Donation ID, Fixed back end destinations

--- src/components/formikForm.js
+++ src/components/formikForm.js
@@ -22,9 +22,9 @@ const validationSchema = Yup.object({
     state: Yup.string("Enter a state").required("State is required").length(2, "Enter state abbrev").uppercase(),
     zip: Yup.string("Enter a zip code").required("Zip code is required").max(10, "Please enter a valid zip code.").min(5, "Please enter a valid zip code.").matches(/\d{5}/, 'Please enter a valid zip code.'),
     amount: Yup.number("Enter a dollar amount").required("Amount is required").positive("Amount must be a positive"),
-    cardNumber: Yup.string("Enter a card number").required("Card number is required").min(19, "Enter a valid card number").max(19, "Enter a valid card number"),
+    cardNumber: Yup.string("Enter a card number").required("Card number is required").min(16, "Enter a valid card number").max(16, "Enter a valid card number"),
     cvv: Yup.string("Enter a cvv").required("cvv is required").min(3, "Enter a valid cvv").max(3, "Enter a valid cvv"),
-    exp: Yup.string("Enter a expiration").required("Expiration is required"),
+    exp: Yup.string("Enter a expiration").required("Expiration is required").max(7, "Enter a valid expiration"),
     frequency: Yup.string("Enter a Frequency").required("Frequency is required"),
 })
 
@@ -70,26 +70,30 @@ class formikForm extends React.Component {
         })
         console.log("current state", this.state)
         const donor = {
-            // donationId: this.state.donationID,
             firstName: this.state.fName,
             lastName: this.state.lName,
-            // email: this.state.email,
-            // address: this.state.address,
-            // apt: this.state.apt,
-            // city: this.state.city,
-            // state: this.state.state,
-            // zip: this.state.zip,
-            // amount: this.state.amount,
-            // frequency: this.state.frequency,
-            // cardNumber: this.state.cardNumber,
-            // cvv: this.state.cvv,
-            // exp: this.state.exp
+            email: this.state.email,
+            address1: this.state.address,
+            unitNumber: this.state.apt,
+            city: this.state.city,
+            state: this.state.state,
+            postalCode: this.state.zip,
+            amount: this.state.amount,
+            frequency: this.state.frequency,
+            cardNumber: this.state.cardNumber,
+            cardSecurityCode: this.state.cvv,
+            expireDate: this.state.exp
         }
 
+        // check here to see what information is being passed in to your axios call
         console.log("Donor info: ", donor)
 
-        axios.post(`http://localhost:8080/apps/DonationsPage/DonationsForm/createDonation`, donor
-            , {
+        // Make sure you set your url to include "apps" instead of "vapps".
+        axios.post(`http://localhost:8080/apps/DonationsPage/DonationsForm/createDonation`, donor,
+
+            // For this project include withCredentials:true in the header of your axios call in order for the
+            // call to bypass the login page
+            {
                 withCredentials: true
             }
         )
